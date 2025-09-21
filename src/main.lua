b6090558a039396368ca53a730ed828c727eff0c
Jean-Pierre <jean.ambrosius@tu-dortmund.de>
2025-09-21 21:26:31 +0200

    Inital Commit

---
+++ src/main.lua
@@ -0,0 +1,9 @@
+
+
+function love.draw()
+    love.graphics.setColor(1,0,0)
+    love.graphics.rectangle("fill",400,100,75,25)
+    love.graphics.setColor(0,1,0)
+    love.graphics.circle("line",0,0,50)
+    love.graphics.setBackgroundColor(0,0,0)
+end
