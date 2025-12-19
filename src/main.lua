200a6480de387369f2cd70a7767b5e4e4bb24bf5
bondivan15 <bondivan@kkg-do.eu>
2025-12-19 10:57:35 +0100

    module

--- src/main.lua
+++ src/main.lua
@@ -1,13 +1,8 @@
--- <- So wird eine Zeile auskommentiert (d.h. der Code/Text danach wird bei "Run" ignoriert.)
---[[ Will man über mehrere Zeilen auskommentieren, so verwendet man
-     zusätzlich eckige Klammern.
-]]--
+require "intro"
+require "junge1"
+require "junge2"
+require "erde"
+require "paradise"
+require "hell"
+require "ende"
 
--- Dies ist eine Methode, die dauerhaft aufgerufen wird - ca. 60 mal pro Sekunde.
-function love.draw()
-    love.graphics.setBackgroundColor(100/255,0,0)              --Farbewerte sind Anteile von 255. Entspricht also rgb(100,0,0)
-    love.graphics.setColor(1,0,0)
-    love.graphics.rectangle("fill",400,100,75,25)    --Ein Rechteck, dessen linke obere Ecke bei den Koordinaten (400|100) ist. Es ist 75x25 Pixel groß.
-    love.graphics.setColor(0,1,0)
-    love.graphics.circle("line",0,0,50)                     --Ein Kreis, dessen Mittelpunkt bei den Koordinaten (0|0) ist. Er hat einen Radius von 50Pixel.
-end
