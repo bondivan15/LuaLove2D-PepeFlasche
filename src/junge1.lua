43b31aaab118612ef55bcad14f13898c7811255e
jannjanu <jannjanu@kkg-do.eu>
2026-01-13 08:36:25 +0100

    männchen

--- src/junge1.lua
+++ src/junge1.lua
@@ -11,3 +11,15 @@ function junge1.zeichne(x, y)
 end
 
 return junge1
+
+function dieb1.zeichne(x, y)
+    love.graphics.setColor(1, 1, 0)
+    love.graphics.circle("fill", x, y, 15)
+    love.graphics.line(x, y, x, y + 50)
+    love.graphics.line(x - 20, y + 20, x + 20, y + 20)
+    love.graphics.line(x, y + 50, x - 15, y + 80)
+    love.graphics.line(x, y + 50, x + 15, y + 80)
+    love.graphics.setColor(1, 1, 1)
+end
+
+return dieb1
